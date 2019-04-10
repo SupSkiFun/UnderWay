c67ff5885666b253c09994c1312f5f0767de3ec1
Joseph Acosta <ja0310@production.int>
2019-04-10 10:03:57 -0700

    updated help

--- Compare.ps1
+++ Compare.ps1
@@ -2,11 +2,11 @@
 .SYNOPSIS
 Compare ESXi Baseline value to value of VMHost
 .DESCRIPTION
-Returns an object of ValueSource, ComparedSystem, Name and Value.  
+Returns an object of Reference, Comparison, ValueSource, Name and Value.  
 Optimally used with a baseline source; see examples.  Alias cvs.
 .PARAMETER Reference
-Baseline object to compare to.  $refobj = import-csv baseline.csv
-
+Baseline object to compare to.  $refObj = import-csv baseline.csv
+Baseline generated with Get-VmHostSetting.  See Example.
 .PARAMETER Difference
 Object value to compare against Reference.  Normally the output of 
 Get-VmHostSetting.  SupSkiFun.VMHostSetting
@@ -14,14 +14,34 @@ Get-VmHostSetting.  SupSkiFun.VMHostSetting
 SupSkiFun.VMHostSetting
 BaseLine Object
 .OUTPUTS
-SupSkiFun.VMHostSetting.Compar
+SupSkiFun.VMHostSetting.Compare
 .EXAMPLE
-Query one VM for RDMs:
+Create Baseline:  $baseline = Get-VMHostSetting -VMHost ESXi03 -Credential $creds
 
-.EXAMPLE
-Query one VMHost for RDMs:
+Modify Baseline:
+    ($baseline| ? Name -eq LicenseKey).Value = "Removed"
+    ($baseline| ? Name -eq Syslog.global.logHost).Value = "Varies"
 
+Create Comparison Object:  $comObj = Get-VMHostSetting -VMHost ESXi777 -Credential $creds
 
+Compare-VMHostSetting -Reference $baseline -Comparison $comObj
+
+Using the alias, storing the object in a variable:  $myVar = cvs -Reference $baseline -Comparison $comObj
+
+----------------------------------------------------------------------------
+
+Optionally Export and later Import:
+
+Export Baseline:  $baseline | Export-Csv D:\6.7\baseline.csv
+
+Import Baseline:  $refObj = Import-Csv D:\6.7\baseline.csv
+
+$comObj = Get-VMHostSetting -VMHost ESXi777 -Credential $creds
+
+Compare-VMHostSetting -Reference $refObj -Comparison $comObj
+
+.LINK
+Get-VMHostSetting
 #>
 
 Function Compare-VMHostSetting
@@ -35,27 +55,29 @@ Function Compare-VMHostSetting
         [psobject]$Reference,
 
         [Parameter(Mandatory = $True , ValueFromPipeline = $True)]
-        [psobject]$Difference
+        [psobject]$Comparison
     )
 
     Process
     {
-        $d1 = Compare-Object -ReferenceObject $Reference -DifferenceObject $Difference -Property Name , Value
-        $dhost = $Difference.Hostname.Split(".")[0]
+        $d1 = Compare-Object -ReferenceObject $Reference -DifferenceObject $Comparison -Property Name , Value
+        $dhost = $Comparison.Hostname.Split(".")[0]
         foreach ($d in $d1)
         {
             if ($d.SideIndicator -eq "=>")
             {
+                $hname = "Comparison"
+            }
+            elseif ($d.SideIndicator -eq "<=") 
+            {
                 $hname = "Reference"
             }
-            else
-            {
-                $hname = $dhost
-            }
+
 
             $lo = [pscustomobject]@{
+                Reference = $Reference.Hostname.Split(".")[0]
+                Comparison = $dhost
                 ValueSource = $hname
-                ComparedSystem = $dhost
                 Name = $d.Name
                 Value = $d.Value
             }
