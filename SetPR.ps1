d347674b52d07cb09026db3071de5799cdc09c95
Joe Acosta <aumjoey@gmail.com>
2019-04-18 21:58:33 -0700

    outline more ideas

--- SetPR.ps1
+++ SetPR.ps1
@@ -2,8 +2,17 @@
 
 <#
 $rdms = (get-vm -Location $MyCLUS Get-HardDisk -DiskType "RawPhysical","RawVirtual").ScsiCanonicalName |select -unique
+
+Loop thru all cluster vmhosts?
+
+
 $tt = $x.storage.core.device.list.Invoke()
-if (($tt | ? device -match $rdms[12]).IsPerenniallyReserved -eq "true") {"yes"} else {"no"}
+
+
+$tt.IsPerenniallyReserved -contains "false"
+then do the loop.  If not just make the object then
+
+object will be VmHost, Device, IsPereniallyReserved
 
 foreach ($rdm in $rdms) {
 >>
@@ -25,8 +34,13 @@ detached                       Unset, ([boolean], optional)
 $x.storage.core.device.setconfig.Invoke($cible)
 
 
-So grab list ($rdms), grab list of devices ($tt) loop list looking for IsPerenniallyReserved -ne true, if -ne true
-make $cible (see below, hopefully don't have to set shared* or detached*), then invoke , then grab list again and report settings.
+So grab list ($rdms), grab list of devices ($tt) ,
+{quick check first for any not perenially reserved?}
+{also make a flag for whethre the list needs to be grabbed again.  $needRescan = $false
+set to $true if SetPerenially reserved is needed / = False}
+loop list looking for IsPerenniallyReserved -ne true, if -ne true
+make $cible (see below, hopefully don't have to set shared* or detached*), then invoke ,
+then grab list again (idf flag set) and report settings.
 
 
  $cible = @{
