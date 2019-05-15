99ed6922261ccfcc3c62fa2eb8f538434d31d294
Joseph Acosta <ja0310@production.int>
2019-05-15 16:09:15 -0700

    Ready!

--- GetTagInfo.ps1
+++ GetTagInfo.ps1
@@ -2,7 +2,8 @@
 .SYNOPSIS
 Retrieves assigned tag information for VM or VMHost
 .DESCRIPTION
-Retrieves assigned tag information for VM or VMHost.
+Retrieves assigned tag information for VM or VMHost, returning an object of
+Entity, Name, Category, and Description.
 .PARAMETER VM
 VMWare PowerCLI VM Object from Get-VM
 VMware.VimAutomation.ViCore.Types.V1.Inventory.VirtualMachine
@@ -10,9 +11,17 @@ VMware.VimAutomation.ViCore.Types.V1.Inventory.VirtualMachine
 VMWare PowerCLI VMHost Object from Get-VMHost
 VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost
 .EXAMPLE
-Example of how to use this cmdlet
+Retrieve tag information of one VM, returning the object into a variable:
+$myVar = Get-VM -Name SYS01 | Get-TagInfo
 .EXAMPLE
-Another example of how to use this cmdlet
+Retrieve tag information of two VMs, returning the object into a variable:
+$myVar = Get-VM -Name SYS02 , SYS03 | Get-TagInfo -Type
+.EXAMPLE
+Retrieve tag information of one VMHost, returning the object into a variable:
+$myVar = Get-VMHost -Name ESX01 | Get-TagInfo
+.EXAMPLE
+Retrieve tag information of two VMHosts, returning the object into a variable:
+$myVar = Get-VMHost -Name ESX02 , ESX03 | Get-TagInfo -Type
 .INPUTS
 VMWare PowerCLI VM or VMHost Object from Get-VM or Get-VMHost:
 VMware.VimAutomation.ViCore.Types.V1.Inventory.VirtualMachine
@@ -20,7 +29,7 @@ VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost
 .OUTPUTS
 PSCUSTOMOBJECT SupSkiFun.Tag.Assignment.Info
 #>
-function Get-Started
+function Get-TagInfo
 {
     [CmdletBinding()]
     [Alias()]
@@ -36,28 +45,54 @@ function Get-Started
 
     Begin
     {
-        $nd = @{Tag = "No Tag Assignment Found"}
+        $h1 = @{Name = "No Tag Assignment Found"}
+        $nd = @{Tag = $h1}
     }
-    
+
     Process
     {
         Function MakeObj
         {
-            param($vdata,$tdata=$nd)
+            param($vdata,$tdata)
 
             $lo = [PSCustomObject]@{
                 Entity = $vdata.Name
-                Tag = $tdata.Tag
-                DisplayName = $resdata.DisplayName
-                WorldID = $resdata.WorldID
-                VMXCartelID = $resdata.VMXCartelID
-                ConfigFile = $resdata.ConfigFile
-                UUID = $resdata.UUID
-                VMHost = $v.VMHost.Name
+                Name = $tdata.Tag.Name
+                Category = $tdata.Tag.Category
+                Description = $tdata.Tag.Description
             }
-            $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.VM.PID.Info')
+            $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.Tag.Assignment.Info')
             $lo
         }
-    }
 
+        Function GetTagInfo($ces)
+        {
+            foreach ($ce in $ces)
+            {
+                $il = $ce | Get-TagAssignment
+                if($il)
+                {
+                    foreach ($i in $il)
+                    {
+                        MakeObj -vdata $ce -tdata $i
+                    }
+                }
+
+                else
+                {
+                    MakeObj -vdata $ce -tdata $nd
+                }
+            }
+        }
+
+        if ($vm)
+        {
+            GetTagInfo($vm)
+        }
+
+        elseif ($vmhost)
+        {
+            GetTagInfo($vmhost)
+        }
+    }
 }
