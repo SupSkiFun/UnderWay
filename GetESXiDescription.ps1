da99da5f2fa74a40b317d475ab139749af51cfab
Joe Acosta <aumjoey@gmail.com>
2019-05-29 20:10:25 -0700

    New AF

---
+++ GetESXiDescription.ps1
@@ -0,0 +1,54 @@
+<#
+.SYNOPSIS
+Retrieves the description of an ESXi image installed on a VMHost.
+.DESCRIPTION
+Retrieves the description of an ESXi image installed on a VMHost.
+Returns an object of HostName, Profile, and Description.
+.PARAMETER VMHost
+Output from VMWare PowerCLI Get-VMHost.  See Examples.
+[VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost]
+.INPUTS
+VMWare PowerCLI VMHost from Get-VMHost:
+[VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost]
+.OUTPUTS
+[PSCUSTOMOBJECT] SupSkiFun.ESXi.Info
+.EXAMPLE
+Retrieve an image description from one VMHost, returning an object into a variable:
+$MyVar = Get-VMHost -Name ESX01 | Get-ESXiDescription
+.EXAMPLE
+Retrieve an image description from two VMHosts, returning an object into a variable:
+$MyVar = Get-VMHost -Name ESX02 , ESX03 | Get-ESXiDescription
+#>
+function Get-ESXiDescription
+{
+    [CmdletBinding()]
+
+    Param
+    (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost[]] $VMHost
+    )
+
+    Process
+    {
+        Function MakeObj
+        {
+            param($vhdata,$resdata)
+
+            $lo = [PSCustomObject]@{
+                HostName = $vhdata
+                Profile = $resdata.Profile
+                Description = $resdata.Description
+            }
+            $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.ESXi.Info')
+            $lo
+        }
+
+        foreach ($vmh in $VMHost)
+        {
+                $xcli = Get-EsxCli -v2 -VMHost $vmh
+                $resp = $xcli.software.profile.get.Invoke()
+                MakeObj -vhdata $vmh.Name -resdata $resp
+        }
+    }
+}
