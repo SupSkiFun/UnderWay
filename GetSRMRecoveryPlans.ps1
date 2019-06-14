fc78eab5ac89c7e09ec49bfcdbee370b6790935d
Joseph Acosta <ja0310@production.int>
2019-06-14 16:28:11 -0700

    code good - needs help

---
+++ GetSRMRecoveryPlans.ps1
@@ -0,0 +1,42 @@
+<#
+$srmED =  $global:DefaultSrmServers.ExtensionData
+$plans = $srmED.Recovery.ListPlans()
+$plans[0].GetInfo().ProtectionGroups.GetInfo()
+$srmED.Protection.ListProtectedVMs()
+$groups = $srmed.Protection.ListProtectionGroups()
+$groups[0].ListProtectedDatastores()
+$groups[0].ListRecoveryPlans()
+$groups[0].     ProtectVms  UnprotectVms    ListProtectedVms
+#>
+
+
+Function Get-SrmRecoveryPlanJoe
+{
+
+    [cmdletbinding()]
+
+    Param
+    (
+        [Parameter()] [string[]] $Name
+    )
+
+    Begin
+    {
+        $srmED =  $global:DefaultSrmServers.ExtensionData
+        $plans = $srmED.Recovery.ListPlans()
+    }
+
+    Process
+    {
+        foreach ($plan in $plans)
+        {
+            $pnom = $plan.GetInfo().Name
+            Add-Member -InputObject $plan -MemberType NoteProperty -Name "Name" -Value $pnom
+        }
+    }
+
+    End
+    {
+        $plans
+    }
+}
