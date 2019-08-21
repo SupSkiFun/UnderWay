041396f86e62e9aa3715a4d485ac31c5107edbfb
Joe Acosta <aumjoey@gmail.com>
2019-08-20 20:23:37 -0700

    switched function to object

--- GetVisorFS.ps1
+++ GetVisorFS.ps1
@@ -1,67 +1,72 @@
-<# 
-.SYNOPSIS 
+Class VFSclass
+{
+    static [pscustomobject] MakeVFSObj ( [string] $Name , [PSObject] $Info )
+    {
+        $obj = [pscustomobject]@{
+            HostName = $name
+            MountPoint = $info.MountPoint
+            PercentFree = $info.Free
+            Maximum = $info.Maximum
+            Used = $info.Used
+            RamDiskName = $info.RamdiskName
+        }
+        $obj.PSObject.TypeNames.Insert(0,'SupSkiFun.ESXi.VisorFS.Info')
+        return $obj
+    }
+}
+<#
+.SYNOPSIS
 
-.DESCRIPTION 
+.DESCRIPTION
 
-.PARAMETER VMHost 
-Output from VMWare PowerCLI Get-VMHost. See Examples. 
-[VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost] 
-.INPUTS 
-VMWare PowerCLI VMHost from Get-VMHost: 
-[VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost] 
-.OUTPUTS 
-PSCUSTOMOBJECT SupSkiFun.ESXi.FileSystem.Info
-.EXAMPLE 
-Returns an object of one VMHost: 
-Get-VMHost -Name ESX01 
-.EXAMPLE 
+.PARAMETER VMHost
+Output from VMWare PowerCLI Get-VMHost. See Examples.
+[VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost]
+.INPUTS
+VMWare PowerCLI VMHost from Get-VMHost:
+[VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost]
+.OUTPUTS
+PSCUSTOMOBJECT SupSkiFun.ESXi.VisorFS.Info
+.EXAMPLE
+Returns an       object of one VMHost:
+Get-VMHost -Name ESX01
+.EXAMPLE
+Need some detaillll ?
 Get-VMHost Name ESX01
-.EXAMPLE 
-Returns an object of two VMHosts, 
-Get-VMHost -Name ESX02 , ESX03 
+.EXAMPLE
+Returns an       object of two VMHosts,
+Get-VMHost -Name ESX02 , ESX03
 .EXAMPLE
 #>
-Function Get-ESXiFileSystem
+Function Get-ESXiVisorFS
 {
     [CmdletBinding()]
     Param
     (
-        [Parameter(ValueFromPipeline=$true)]
-        [VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost[]]$VMHost
+        [Parameter(ValueFromPipeline = $True, Mandatory = $True)]
+        [VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost[]]$VMHost,
+        [string] $Pattern
     )
-
-    Begin
-    {
-        Function MakeObj
-        {
-            param ($Name, $Info)
-
-            $lo = [pscustomobject]@{
-                HostName = $name
-                MountPoint = $info.MountPoint
-                PercentFree = $info.Free
-                Maximum = $info.Maximum
-                Used = $info.Used
-                RamDiskName = $info.RamdiskName
-            }
-            $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.ESXi.FileSystem.Info')
-            $lo
-        }
-
-    }
 
     Process
     {
         foreach ($vmh in $vmhost)
         {
             $x2 = Get-EsxCli -V2 -VMHost $vmh
-            # Maybe just check them all? - use parameter for specific?
-            $z2 = $x2.system.visorfs.ramdisk.list.Invoke()
-            #$z2 = $x2.system.visorfs.ramdisk.list.Invoke().where({$_.MountPoint -match "var" -or $_.MountPoint -match "tmp"})
-            # $z2 = $x2.system.visorfs.ramdisk.list.Invoke()   Modify to use $fs array
+
+            if (-not($pattern))
+            {
+                $z2 = $x2.system.visorfs.ramdisk.list.Invoke()
+            }
+            else
+            {
+                $z2 = $x2.system.visorfs.ramdisk.list.Invoke().where({$_.MountPoint -match $Pattern})
+            }
+
             foreach ($z in $z2)
             {
-                MakeObj -Name $vmh.Name -Info $z
+                $lo = [VFSclass]::MakeVFSObj($vmh.Name , $z)
+                $lo
             }
         }
     }
