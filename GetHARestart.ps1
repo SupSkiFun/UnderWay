68ddfe91d3816facf43156f80d89b5cc0b311b6d
Joseph Acosta <ja0310@production.int>
2020-01-28 16:24:11 -0800

    Added Help

--- GetHARestart.ps1
+++ GetHARestart.ps1
@@ -18,19 +18,37 @@ Class NewVClass
     }
 }
 
-<#
-    TODO:  
-        Paramaterize the Begin statement ZZ  MaxSamples -Start
-        Add Help
-        Split out MakeObj and ParseInfo?
-
+<# 
+.SYNOPSIS 
+Finds VM associated with an HA Restart
+.DESCRIPTION 
+Returns an object of VM, Notes, NewHost, DateTime and FullMessage for each VM
+affected by an HA Restart
+.PARAMETER Days
+Number of Days back (from current day) to check.  Defaults to 1 if not specified.
+.PARAMETER MaxSamples
+Number of Samples to check.  Defaults to 100000 if not specified.
+.OUTPUTS 
+[pscustomobject] SupSkiFun.HA.Restart.Info
+.EXAMPLE 
+Return HA Restart object into a variable, using default values: 
+$MyVar = Get-HARestartInfo
+.EXAMPLE 
+Return HA Restart object into a variable, searching back 3 Days, with a maximum of 5000 samples: 
+$MyVar = Get-HARestartInfo -Days 3 -MaxSamples 5000
 #>
-
 Function Get-HARestartInfo
 {
+    [CmdletBinding()]
+    param 
+    (
+        [Int16] $Days = 1 ,
+        [int32] $MaxSamples = 1000000
+    )
+
     Begin
     {
-        $zz = Get-VIEvent -MaxSamples 100000 -Start (Get-Date).AddDays(-1) -Type Warning | 
+        $zz = Get-VIEvent -MaxSamples $MaxSamples -Start (Get-Date).AddDays(-$Days) -Type Warning | 
             Where-Object -Property FullFormattedMessage -match "vSphere HA restarted" | 
                 Select-Object -Property CreatedTime , FullFormattedMessage | 
                     Sort-Object -Property CreatedTime -Descending
@@ -53,14 +71,7 @@ Function Get-HARestartInfo
     }
 }
 
-
-
-
-
 <#
-
-Example output of $z.FullFormattedMessage above looks akin to:
-
-vSphere HA restarted virtual machine MyFine on host ESX-Server-O1.example.com in cluster PROD-17
-
+    Example output of $z.FullFormattedMessage above looks akin to:
+    vSphere HA restarted virtual machine MyFine on host ESX-Server-O1.example.com in cluster PROD-17
 #>
