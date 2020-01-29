709006d735f29b264d63a4710881d6f1429f31d5
Joseph Acosta <ja0310@production.int>
2020-01-29 15:45:28 -0800

    new

---
+++ TryCatch.ps1
@@ -0,0 +1,30 @@
+Function TestE
+{
+    param ($Value)
+    
+    $d = "."
+    if ($value.ToString().Contains($d))
+    {
+        $a = $value.Split(".")
+        $a[0],($a[1][0..3] -join "") -join "." 
+    }
+    else 
+    {
+        $value        
+    }
+}
+
+<#
+
+    PowerShell Equivalent of
+    
+    s = str(s)
+    try:
+        i = s.index(".")
+    except:
+        return s
+    else:
+        return (s[:(i+5)])
+
+
+#>
