4f626a93ac8cb8885306fb6ce200bc00769c57e6
Joseph Acosta <ja0310@production.int>
2020-02-10 17:42:06 -0800

    start

--- AlarmFun.ps1
+++ AlarmFun.ps1
@@ -1,3 +1,15 @@
+$almg = Get-View AlarmManager
+$filt = [VMware.Vim.AlarmFilterSpec]::new()
+
+$filt.Status += [VMware.Vim.ManagedEntityStatus]::red
+$filt.TypeEntity = [VMware.Vim.AlarmFilterSpecAlarmTypeByEntity]::entityTypeVm
+$filt.TypeTrigger = [vmware.vim.AlarmFilterSpecAlarmTypeByTrigger]::triggerTypeEvent
+
+$alarmMgr.ClearTriggeredAlarms($filter)
+
+<# 
+https://communities.vmware.com/thread/623890
+
 $alarmMgr = Get-View AlarmManager
  
 $filter = New-Object VMware.Vim.AlarmFilterSpec
@@ -6,3 +18,6 @@ $filter.TypeEntity = [VMware.Vim.AlarmFilterSpecAlarmTypeByEntity]::entityTypeVm
 $filter.TypeTrigger = [vmware.vim.AlarmFilterSpecAlarmTypeByTrigger]::triggerTypeEvent
 
 $alarmMgr.ClearTriggeredAlarms($filter)
+
+
+#>
