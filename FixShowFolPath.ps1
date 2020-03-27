63a2c836781458bdc68ba3b7344bb6c225aed086
Joseph Acosta <ja0310@production.int>
2020-03-27 15:26:45 -0700

    two fixes

---
+++ FixShowFolPath.ps1
@@ -0,0 +1,32 @@
+Function Show-FolderPathTesT #Remove!
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true , ValueFromPipeline = $true)]
+        [VMware.VimAutomation.ViCore.Types.V1.Inventory.Folder[]] $Folder
+    )
+
+    Process
+    {
+        foreach ($sn in $folder)
+        {
+            $sne = $sn.ExtensionData
+            $fp = $sn.name
+            while ($sne.Parent)
+            {
+                $sne = Get-View $sne.Parent
+                $fp  = Join-Path -Path $sne.name -ChildPath $fp
+            }
+
+            $lo = [PSCustomObject]@{
+                Name = $sn.Name
+                Id = $sn.id
+                Path = $fp
+                Type = $sn.Type.ToString()
+            }
+            $lo
+            $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.VSphereFolder.Info')
+        }
+    }
+}
---
+++ FixTag.ps1
@@ -0,0 +1,63 @@
+function Get-TagInfoTEST #Remove!
+{
+    [CmdletBinding()]
+
+    param
+    (
+        [Parameter(ParameterSetName = "VM" , ValueFromPipeline = $true)]
+                [VMware.VimAutomation.ViCore.Types.V1.Inventory.VirtualMachine[]] $VM,
+
+        [Parameter(ParameterSetName = "VMHost" , ValueFromPipeline = $true)]
+                [VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost[]] $VMHost
+    )
+
+    Begin
+    {
+        $nd = @{Tag = @{Name = "No Tag Assignment Found"}}
+    }
+
+    Process
+    {
+        Function MakeObj
+        {
+            param($vdata,$tdata)
+            $lo = [PSCustomObject]@{
+                Entity = $vdata
+                Name = $tdata.Tag.Name
+                Category = $tdata.Tag.Category.Name
+                Description = $tdata.Tag.Description
+            }
+            $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.Tag.Assignment.Info')
+            $lo
+        }
+
+        Function GetTagInfo($ces)
+        {
+            foreach ($ce in $ces)
+            {
+                $il = $ce | Get-TagAssignment
+                if($il)
+                {
+                    foreach ($i in $il)
+                    {
+                        MakeObj -vdata $ce.Name.ToSTring() -tdata $i
+                    }
+                }
+                else
+                {
+                    MakeObj -vdata $ce.Name.ToSTring() -tdata $nd
+                }
+            }
+        }
+
+        if ($vm)
+        {
+            GetTagInfo($vm)
+        }
+
+        elseif ($vmhost)
+        {
+            GetTagInfo($vmhost)
+        }
+    }
+}
