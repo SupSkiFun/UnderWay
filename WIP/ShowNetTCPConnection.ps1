8216aa24f0eca010c927e3336d565580ac49d1a6
Joe Acosta <aumjoey@gmail.com>
2020-05-17 17:19:44 -0700

    close

--- WIP/ShowNetTCPConnection.ps1
+++ WIP/ShowNetTCPConnection.ps1
@@ -1,45 +1,90 @@
 #Needs Error handling / Parameters / Testing / Code Formatiing
-$pp = Get-NetTCPConnection -State Established |? {$_.RemoteAddress -notmatch "^127" -and $_.RemoteAddress -notmatch "::1" }
-$op = $pp.OwningProcess | Select -Unique
-$qp = Get-Process -PID $op | Select Id , Name , Description, Company, Path
-$uri = 'http://api.ipapi.com/'  # Keep Trailing Slash - code around that
-#$cle = 'PUT API Key Here'
-$cle = Get-Content apikey.txt
-$headers = @{
-	access_key = $cle
-	output = 'json'
-	fields = 'ip,country_name,region_name,city'  # Intentionally no Space
+
+Function GetProcInfo
+{
+    $script:pp = Get-NetTCPConnection -State Established |
+        Where-Object {$_.RemoteAddress -notmatch "^127" -and $_.RemoteAddress -notmatch "::1" }
+    $op = $pp.OwningProcess |
+        Select-Object -Unique
+    $script:qp = Get-Process -PID $op |
+        Select-Object -Property Id , Name , Description , Company , Path
+}
+
+Function SetParams
+{
+    #$cle = 'PUT API Key Here'  Or ...
+    $cle = Get-Content apikey.txt -ErrorAction SilentlyContinue
+    if ($cle)
+    {
+        $script:uri = 'http://api.ipapi.com/'  # Keep Trailing Slash - code around that
+        $script:body = @{
+            access_key = $cle
+            output = 'json'
+            fields = 'ip,country_name,region_name,city'  # Intentionally no Space
+        }
+    }
+    else
+    {
+        $script:cleOK = $false
+    }
 }
 
 Function GetIpInfo
 {
     param ($IP)
-    # Need Error Handling in this
-    $r2 = Invoke-RestMethod -Uri ($uri+$ip) -body $headers
-    $r2
-}
-
-foreach ($p in $pp)
-{
-    # IF NOT $rp MAKE A NO DATA VALUE?  oR JUST LEAVE?
-    $rp = $qp.Where({$p.OwningProcess.ToString() -eq $_.id.ToString()})
-    $ip = $p.RemoteAddress
-    $ipinfo = GetIpInfo -IP $ip
-
-    $r2 = Invoke-RestMethod -Uri ($uri+$ip) -body $headers
-        $lo = [pscustomobject]@{
-        LocalAddress = $p.LocalAddress
-        LocalPort = $p.LocalPort
-        RemoteAddress = $ip
-        RemotePort = $p.RemotePort
-        Setting = $p.AppliedSetting.ToString()
-        Process = $rp.id
-        ProcessName = $rp.Name
-        Description = $rp.Description
-        Company = $rp.Company
-        Path = $rp.Path
-        Locale = $ipinfo
+    $IRP = @{
+        Uri = ($uri+$ip)
+        Body = $body
+        Method = "Get"
+        ErrorAction = "SilentlyContinue"
+        ErrorVariable = "err"
     }
-    $lo
-    $rp = $null
+    $r2 = Invoke-RestMethod @IRP
+    if ($err)
+    {
+        $msg = "Error obtaining IP Info from API"
+        $msg
+    }
+    else
+    {
+        $r2
+    }
 }
+
+
+Function MakeObj
+{
+    foreach ($p in $pp)
+    {
+        # IF NOT $rp MAKE A NO DATA VALUE?  OR JUST LEAVE?
+        $rp = $qp.Where({$p.OwningProcess.ToString() -eq $_.id.ToString()})
+        $ip = $p.RemoteAddress
+        if ($cleOK)
+        {
+            $ipinfo = GetIpInfo -IP $ip
+        }
+        else
+        {
+            $ipinfo = "API Key Not Found"
+        }
+        $lo = [pscustomobject]@{
+            LocalAddress = $p.LocalAddress
+            LocalPort = $p.LocalPort
+            RemoteAddress = $ip
+            RemotePort = $p.RemotePort
+            Setting = $p.AppliedSetting.ToString()
+            Process = $rp.id
+            ProcessName = $rp.Name
+            Description = $rp.Description
+            Company = $rp.Company
+            Path = $rp.Path
+            Locale = $ipinfo
+        }
+        $lo
+        $rp = $null
+    }
+}
+
+GetProcInfo
+SetParams
+MakeObj
