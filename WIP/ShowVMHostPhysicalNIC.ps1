b483348ea717c721d581ef3682c72b873ed06013
Joe Acosta <supskifun@gmail.com>
2021-04-23 14:24:01 -0700

    added help

--- WIP/ShowVMHostPhysicalNIC.ps1
+++ WIP/ShowVMHostPhysicalNIC.ps1
@@ -8,16 +8,38 @@ Class FizNic
             Speed = $n.extensiondata.linkspeed.SpeedMB
             Duplex = $n.extensiondata.linkspeed.Duplex
         }
+        $lo.PSObject.TypeNames.Insert(0,'SupSkiFun.VMHost.Physical.NIC.Info')
         return $lo
     }
 }
+
+<#
+.SYNOPSIS
+Returns VMHost physical NIC properties
+.DESCRIPTION
+Returns an object of VMHost physical NIC properties
+.PARAMETER VMHost
+VMWare PowerCLI VMHost Object from Get-VMHost
+VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost
+.INPUTS
+VMHost Object from Get-VMHost:
+VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost
+.OUTPUTS
+PSCUSTOMOBJECT SupSkiFun.VMHost.Physical.NIC.Info
+.EXAMPLE
+Query one VMHost for Physical NIC Info:
+Get-VMHost -Name ESXi17 | Show-VMHostPhysicalNIC
+.EXAMPLE
+Query multiple hosts for Physical NIC Info returning the object into a variable:
+$MyObj = Get-VMHost -Name ESX0* | Show-VMHostPhysicalNIC
+#>
 
 Function Show-VMHostPhysicalNIC
 {
     [cmdletbinding()]
     Param
     (
-        [Parameter(ValueFromPipeline = $true)]
+        [Parameter(ValueFromPipeline = $true , Mandatory = $true)]
         [VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost[]] $VMHost
     )
 
